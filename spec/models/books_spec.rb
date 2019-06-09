ee3d514788799d568cea04dc7de958628455d929
JeanVassalo <jeanvassaloferreira@gmail.com>
2019-06-08 22:04:50 -0300

    att

--- spec/models/books_spec.rb
+++ spec/models/books_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
+require 'spec_helper'
+require '../app/models/books'
 
 RSpec.describe Books, type: :model do
   context 'validation tests' do
+    it {should validate_presence_of :name}
     it "should pass" do
       books=Books.new(name:'Calculo',description:'Become the master of Calculo in one week',price:'100',author:'Guillermo',category:'Math',cover:'n faço ideia da capa').save
       expect(books).to eq(true)
@@ -18,9 +21,9 @@ RSpec.describe Books, type: :model do
       books=Books.new(name:'Calculo',description:'Become the master of Calculo in one week',author:'Guillermo',category:'Math',cover:'n faço ideia da capa').save
       expect(books).to eq(false)
     end
-    it 'should verif if price type' do
+    it 'should verif price type' do
       books=Books.new(name:'Calculo',description:'Become the master of Calculo in one week',price:'a',author:'Guillermo',category:'Math',cover:'n faço ideia da capa').save
-      expect(books).to eq(false)
+      expect(books.price).to eq('100')#AQUI DEVE VERIFICAR SE É INTEIRO, N SEI FAZER AINDA
     end
     it "should verif cover size" do
       books=Books.new(name:'Calculo',description:'Become the master of Calculo in one week',price:'100',author:'Guillermo',category:'Math',cover:'n faço ideia da capa').save#?
@@ -28,6 +31,10 @@ RSpec.describe Books, type: :model do
     end
     it "should verif cover type" do
       books=Books.new(name:'Calculo',description:'Become the master of Calculo in one week',price:'100',author:'Guillermo',category:'Math',cover:'n faço ideia da capa').save#?
+      expect(books).to eq(false)
+    end
+    it "should have cover" do
+      books=Books.new(name:'Calculo',description:'Become the master of Calculo in one week',price:'100',author:'Guillermo',category:'Math').save#?
       expect(books).to eq(false)
     end
 
