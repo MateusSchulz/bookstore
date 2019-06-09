47bdb05ba314287c648e1bfe859573ac83609af0
NandoLinhares <fernandolinhares1920@globo.com>
2019-06-09 23:04:59 +0000

    att umas paradas

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe Book, type: :model do
         it { should validate_presence_of(:description) }
         it { should validate_presence_of(:price) }
         it { should validate_numericality_of(:price) }
+        it { should have_attached_file(:avatar) }
+        #it { should validate_attachment_presence(:avatar) }
+        #it { should validate_attachment_content_type(:avatar).allowing('image/png', 'image/gif').rejecting('text/plain', 'text/xml') }
+        #it { should validate_attachment_size(:avatar).less_than(2.megabytes) }
     end
 end
