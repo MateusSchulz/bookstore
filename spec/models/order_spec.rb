e7485ea078a9db0554a16e43134cdfa8ec549ed1
Isabela Correia <belacoper@gmail.com>
2019-06-10 14:19:56 -0300

    Test presence of class order

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
+  it {should validate_presence_of :user_id}
+  it {should validate_presence_of :name}
+  it {should validate_presence_of :address}
+  it {should validate_presence_of :city}
+  it {should validate_presence_of :state}
+  it {should validate_presence_of :total_price}
+
   describe 'associations' do
   	it { should have_many(:line_items) }
   	it { should belong_to(:user) }
